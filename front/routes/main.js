57dcfad2d09acf363c3a7a3ff7c37ac182b83d84
wjdguswn1203 <wjdguswn1203@naver.com>
2024-04-30 13:35:18 +0900

    mysql api select 출력

--- front/routes/main.js
+++ front/routes/main.js
@@ -1,23 +1,38 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require('body-parser');
 const pool = require("../pool");
+const axios = require("axios");
+const app = express();
 
-const app = express()
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.redirect('index.html');
 })
 
-app.get('/selectPyeup', async (req, res) => {
-    const [data, fileds] = await pool.query("select * from pyeup");
-    // console.log(data);
-
-    res.redirect('selectPyeup.html');
-})
+app.get("/selectPyeup", async (req, res) => {
+    const year = req.query.year;
+    try {
+        let tmp = await pool.query("select * from pyeup where year=?", [year]);
+        console.log(tmp);
+        console.log(tmp[0].length);
+        if (tmp[0].length == 0) {
+            const response = await axios.get("http://0.0.0.0:3500/insertSQL?year=" + String(year));
+            tmp = response.data;
+        }
+        tmp = await pool.query("select * from pyeup where year=? order by count desc",[year]);
+        var result = {
+            "result code": res.statusCode,
+            result: tmp[0],
+        };
+        res.send(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+});
 
 module.exports = app;
